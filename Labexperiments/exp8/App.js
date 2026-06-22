ef617fa4597c28f5151b41fcac31a38ece1aca2f
Navya Bhadana <navyabhadana766@gmail.com>
2026-06-22 23:58:39 +0530

    updated changes

--- Labexperiments/exp8/App.js
+++ Labexperiments/exp8/App.js
@@ -7,9 +7,9 @@ return (
  <h1>React Counter</h1>
  <h2 className={count > 0 ? 'pos' : count < 0 ? 'neg' : ''}>{count}</h2>
  <div className="buttons">
- <button onClick={() => setCount(count + 1)}>Increment (+)</button>
- <button onClick={ ()=> setCount(count - 1)}>Decrement (-)</button>
- <button onClick={ ()=> setCount(0)}>Reset</button>
+ <button onClick={    ()   => setCount(count + 1)}>Increment (+)</button>
+ <button onClick={    ()  => setCount(count - 1)}>Decrement (-)</button>
+ <button onClick={    ()=> setCount(0)}>Reset</button>
  </div>
  </div>
  );
