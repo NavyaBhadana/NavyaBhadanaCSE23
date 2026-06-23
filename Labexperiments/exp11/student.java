11486cfd0771ffaf36ba7273e723cef0d680df3b
Navya Bhadana <navyabhadana766@gmail.com>
2026-06-23 09:09:22 +0530

    changes

--- Labexperiments/exp11/student.java
+++ Labexperiments/exp11/student.java
@@ -20,11 +20,11 @@ public class Student {
 
     // Getter and Setter methods
     public int getId() {
-        return id ;
+          return id ;
     }
 
     public void setId( int id) {
-        this.id = id;
+             this.id    = id;
     }
 
     public String getName() {
